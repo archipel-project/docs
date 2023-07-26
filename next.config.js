a3d590584be6b39934c9892832731c0de1e366c2
Louis Bailleau <louis.bailleau@free.fr>
2023-07-26 23:39:20 +0200

    fix: 🔧 update next config

--- next.config.js
+++ next.config.js
@@ -6,4 +6,11 @@ const config = {
 
 const withNextra = require('nextra')(config)
 
-module.exports = withNextra()
+module.exports = {
+  ...withNextra(),
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
+}
---
+++ out/_next/static/Vn-pxV9wuvFyMDqIzytf5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*/_meta",destination:"/404"}],fallback:[]},"/":["static/chunks/352-f05205912a634b11.js","static/chunks/pages/index-c202a6ebc16794d1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
